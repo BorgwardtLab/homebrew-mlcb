accf17200a28e379d7ccd44eeee8c7ddf1746b07
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-11-23 17:23:10 +0100

    Provided a simple formula for installing Ripser

---
+++ Formula/ripser.rb
@@ -0,0 +1,20 @@
+class Ripser < Formula
+  desc "Ripser is a lean C++ code for the computation of Vietoris–Rips persistence barcodes. It can do just this one thing, but does it extremely well."
+  homepage "https://github.com/Ripser/ripser"
+  version "master"
+  url "https://github.com/Ripser/ripser/archive/master.zip"
+  depends_on "clang" => :build
+
+  def install
+    system "make"
+
+    # Ensures that the path exists; since the default Makefile does not
+    # provide an `install` target, let's just do it manually.
+    bin.mkpath
+    bin.install "ripser"
+  end
+
+  test do
+    system "#{bin}/ripser",  "--help"
+  end
+end
